1c476a9a60664d6025665b12df4622a8971286fd
Kenny Tang <kenny.t4ng@gmail.com>
2025-11-23 10:37:31 +0700

    Update solution to be more efficient

--- database-technology/assignment-1/murder_mystery.sql
+++ database-technology/assignment-1/murder_mystery.sql
@@ -7,46 +7,40 @@ WHERE type = "murder"
 -- Security footage shows that there were 2 witnesses. The first witness lives at the last house on "Northwestern Dr". 
 -- The second witness, named Annabel, lives somewhere on "Franklin Ave".
 
-SELECT * 
-FROM person
-WHERE address_street_name = "Northwestern Dr"
-ORDER BY address_number;
+SELECT p.*, i.transcript
+FROM person p
+JOIN interview i ON p.id = i.person_id
+WHERE
+    (p.address_street_name = 'Northwestern Dr' AND
+     p.address_number = (
+       SELECT MAX(address_number)
+       FROM person
+       WHERE address_street_name = 'Northwestern Dr'
+     ))
+  OR
+    (p.name LIKE '%Annabel%' AND p.address_street_name = 'Franklin Ave');
 
--- 14887 (id)
--- Morty Schapiro (name) 
--- 118009 (license_id)
--- 4919 (address_number)
--- Northwestern Dr (address_street_name)
--- 111564949 (ssn)
-
-SELECT * 
-FROM interview
-WHERE person_id = "14887";
+-- 14887	Morty Schapiro	118009	4919	Northwestern Dr	111564949
 
 -- Transcript from the Interview
 -- I heard a gunshot and then saw a man run out. He had a "Get Fit Now Gym" bag. 
 -- The membership number on the bag started with "48Z". Only gold members have those bags. 
 -- The man got into a car with a plate that included "H42W".
 
-SELECT * 
-FROM get_fit_now_member
-WHERE membership_status = "gold" 
-	AND id LIKE '48Z%';
+-- 16371	Annabel Miller	490173	103	Franklin Ave	318771143
 
--- 48Z7A	28819	Joe Germuska	20160305	gold
--- 48Z55	67318	Jeremy Bowers	20160101	gold
+-- Transcript from the Interview
+-- I saw the murder happen, and I recognized the killer from my gym when I was working out last week on January the 9th.
 
-SELECT * 
-FROM drivers_license
-WHERE plate_number like '%H42W%';
+SELECT m.*, p.name, d.plate_number
+FROM get_fit_now_member m
+    JOIN person p ON m.person_id = p.id
+    JOIN drivers_license d ON p.license_id = d.id
+WHERE m.membership_status = 'gold'
+    AND m.id LIKE '48Z%'
+    AND d.plate_number LIKE '%H42W%';
 
--- 183779	21	65	blue	blonde	female	H42W0X	Toyota	Prius
--- 423327	30	70	brown	brown	male	0H42W2	Chevrolet	Spark LS
--- 664760	21	71	black	black	male	4H42WR	Nissan	Altima
-
-SELECT * 
-FROM person
-WHERE id = 28819 or id = 67318;
+-- 48Z55	67318	Jeremy Bowers	20160101	gold	Jeremy Bowers	0H42W2
 
 -- MURDER 1 DONE ( Jeremy Bowers )-- 
 
@@ -59,47 +53,19 @@ WHERE person_id = 67318;
 -- She has red hair and she drives a Tesla Model S. 
 -- I know that she attended the SQL Symphony Concert 3 times in December 2017. 
 
-SELECT * 
-FROM drivers_license
-where car_make = "Tesla"
-	AND car_model = "Model S"
-	AND gender = "female"
-	AND hair_color = "red";
+SELECT p.*, d.car_make, d.car_model, d.hair_color, d.gender, f.event_name, f.date
+FROM drivers_license d
+    JOIN person p ON p.license_id = d.id
+    JOIN facebook_event_checkin f ON f.person_id = p.id
+WHERE d.car_make = 'Tesla'
+    AND d.car_model = 'Model S'
+    AND d.gender = 'female'
+    AND d.hair_color = 'red'
+    AND f.event_name = 'SQL Symphony Concert'
+    AND f.date BETWEEN '20171201' AND '20171231'
+GROUP BY p.id
+HAVING COUNT(*) = 3;
 
--- 202298	68	66	green	red	female	500123	Tesla	Model S
--- 291182	65	66	blue	red	female	08CM64	Tesla	Model S
--- 918773	48	65	black	red	female	917UU3	Tesla	Model S
-
-
-SELECT * 
-FROM person
-WHERE license_id = 918773 
-    OR license_id = 291182
-    OR license_id = 202298;
-
--- 78881	Red Korb	918773	107	Camerata Dr	961388910
--- 90700	Regina George	291182	332	Maple Ave	337169072
--- 99716	Miranda Priestly	202298	1883	Golden Ave	987756388 
-
-SELECT * 
-FROM income
-WHERE ssn = 961388910 
-    OR ssn = 337169072
-    OR ssn = 987756388;
+-- 99716	Miranda Priestly	202298	1883	Golden Ave	987756388	Tesla	Model S	red	female	SQL Symphony Concert	20171229
 
 -- MURDER 2 DONE ( Miranda Priestly ) -- 
-
-SELECT * 
-FROM person
-WHERE address_street_name = "Franklin Ave"
-	AND name LIKE "%Annabel%";
-
--- 16371	Annabel Miller	490173	103	Franklin Ave	318771143
-
-SELECT * 
-FROM interview
-WHERE person_id = 16371;
-
--- I saw the murder happen, and I recognized the killer from my gym when I was working out last week on January the 9th.
-
-
